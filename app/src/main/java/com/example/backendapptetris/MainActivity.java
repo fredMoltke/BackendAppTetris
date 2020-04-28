34876a43dcd37271bc353a3b64038615e9b0f522
Frederik <frederik.moltke@hotmail.com>
2020-04-28 20:54:39 +0200

    Første firebase commit

--- app/src/main/java/com/example/backendapptetris/MainActivity.java
+++ app/src/main/java/com/example/backendapptetris/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.backendapptetris;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final String KEY_NAVN = "navn";
+    private static final String KEY_SCORE = "score";
+
+    private Button button;
+    private TextView textView;
+    private EditText editText;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+
+    }
+
+    public void saveNote(View v) {
+        String navn = editText.getText().toString();
+        Map<String, Object> score = new HashMap<>();
+        score.put(KEY_NAVN, navn);
+        score.put(KEY_SCORE, "1234");
+        db.collection("Highscores").document("Foerste Bruger").set(score)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Score gemt.", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Fejl", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, e.toString());
+            }
+        });
     }
 }
